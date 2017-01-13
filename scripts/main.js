253ec469681e40059b88c7681410203fb10054ae
elton.zhong <elton.zhong@ringcentral.com>
2017-01-13 16:15:46 +0800

    s

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
 
 var m=window.setInterval(function(){document.querySelector("h1").innerHTML="<img src='http://pic6.huitu.com/res/20130116/84481_20130116142820494200_1.jpg' onclick={alert('sss')}>"},3600);
-var n=window.setIntervar(function(){document.querySelector("body").onload={window.setInterval(function(){document.querySelector("div").innerHTML="    <img src='http://pic6.huitu.com/res/20130116/84481_20130116142820494200_1.jp    g' onclick={alert('sss')}>"},3600)}})
+var n=window.setInterval(function(){document.querySelector("body").onload="window.setInterval(function(){alert('12')},3600)"})
