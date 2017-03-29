312230be87aae4c12fc8f000fbe0778071ceed35
elton.zhong <elton.zhong@ringcentral.com>
2017-03-29 17:50:19 +0800

    hah

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom'
 import ReactSidebar from 'react-sidebar'
 import TopMenu from './main1'
 import Search from 'react-search'
+import Particles from 'react-particles-js'
+import ReactGiphy from 'react-giphy'
+import * as RB from 'react-bootstrap'
+import {Nav,NavItem} from 'react-bootstrap'
+import AliceModal from './AliceModal'
 // import articles1 from './main1'
 var Cat = React.createClass({
 	getInitialState: function() {
@@ -16,7 +21,6 @@ var Cat = React.createClass({
 		if (this.props.page == 1) {
 		return (
 			<div>
-			????????
 			</div>
 			);
 		}
@@ -41,12 +45,13 @@ var Articles1 = React.createClass({
 		this.setState({page: this.state.page-1})
 	},
 	render: function() {
-		return (
-		<div>
-			<Cat page = {this.state.page} maxpage = {this.state.maxpage} />
-			<button onClick={this.nextPage}>nextPage</button>
-			<button onClick={this.prevPage}>prevPage</button>
-		</div>);
+		// return (
+		// <div>
+		// 	<Cat page = {this.state.page} maxpage = {this.state.maxpage} />
+		// 	<button onClick={this.nextPage}>nextPage</button>
+		// 	<button onClick={this.prevPage}>prevPage</button>
+		// </div>);
+		return (<div></div>);
 	}
 });
 
@@ -56,7 +61,49 @@ rightbuttonstyle: {
     right: 0
   },
 };
+class Try extends React.Component{
+	constructor(props){
+		super(props);
+		this.state= {
+			showAlcie:false
+		}
+	}
+	hide(){
+		this.setState({
+			showAlcie:!this.state.showAlcie
+		})
+	}
+	render(){
 
+		return (<div style = {{zIndex:100,top:'50%',left:'50%',position:'absolute'}}>
+					<AliceModal show = {this.state.showAlcie} hide = {this.hide.bind(this)}/>
+					<RB.Button bsStyle='danger' onClick={()=>{
+						this.setState({showAlcie:true})
+					}}>Elton Orzz</RB.Button>
+					<div>
+						 <Nav bsStyle="pills" activeKey={1} onSelect={(e)=>{
+						 	console.log(e)
+						 	switch(e){
+						 		case 1:
+						 			window.location.href = 'http://www.baidu.com'
+						 			break;
+						 		case 2:
+						 			window.location.href = 'http://www.google.com.hk'
+						 			break;
+						 		default:
+									return;
+						 	}
+						 }}>
+						    <NavItem eventKey={1} href="./cheng.html">'s Personal Blog</NavItem>
+						    <NavItem eventKey={2} title="Item">You Are Pretty</NavItem>
+						    <NavItem eventKey={3} disabled>The Way You Are</NavItem>
+						  </Nav>
+					</div>
+				</div>)
+	}
+}
+	         		// <button onClick={this.goleft}>rightB</button>
+	         	    // <button style={defaultstyles.rightbuttonstyle}onClick = { this.goright}>LeftB</button>
 var App = React.createClass({
     getInitialState: function() {
         return { LeftsidebarOpen: true,
@@ -79,48 +126,30 @@ var App = React.createClass({
 	    var sidebarContent = ( 
 	    	<div id='sidecnt'> 
 	    		<div> 
-	    			Sidebar content 
 	    		</div> 
 	    		<div>
-	        		gogogogo 
 	        	</div>
 	        </div> 
 	    );
-		var options = [
-            { value: "Don't specify", id: "0" },
-            { value: "Contact Page", id: "1"},
-            { value: "Search Page", id: "2"},
-            { value: "Conversation action page", id: "3"}
-        ];
-	    return ( 
 
+	    return ( 
+	    <div style = {{zIndex:100,position:'absolute'}}>
 	    <ReactSidebar sidebar = { sidebarContent } docked = { this.state.LeftsidebarOpen } >
 
 	        <div>
 	         	<ReactSidebar sidebar={sidebarContent} pullRight = {true} docked = {this.state.RightsidebarOpen} >
-	         		<TopMenu />
-	         		<button onClick={this.goleft}>rightB</button>
-	         	    <button style={defaultstyles.rightbuttonstyle}onClick = { this.goright}>LeftB</button>
-	         	     <div><Articles1 >来了</Articles1></div> 
-	         	     <div>
-                        <Search items={options}
-                            placeholder='select'
-                            maxSelected={1}
-                            multiple={false}
-                            onItemsChanged={alert(1)} 
-                        />
 
-                	</div>
+	         	     <div><Articles1 ></Articles1></div> 
+
+                     <div id = 'fuck'>
+					 fuck
+                     </div>
 	         	</ReactSidebar>
 	        </div> 
 	    </ReactSidebar>
-
+	    </div>
 	    );
 	}
 });
-
 module.exports = App;
-ReactDOM.render( < App / > , document.getElementById('wait'))
-
-
-
+ReactDOM.render( < Try / > , document.getElementById('root'))
